266e1671f38108b020e039a58fb165a250090407
thanawatgulati <thanawatgulatijames@gmail.com>
2020-08-14 15:17:39 +0700

    pointer

--- hello_world.go
+++ hello_world.go
@@ -1,20 +1,26 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Variable
-	a := map[string]string{
-		"Hello": "Gopher",
-		"Name":  "James",
-		"X":     "Hello from the other side.",
-	}
+	// a := map[string]string{
+	// 	"Hello": "Gopher",
+	// 	"Name":  "James",
+	// 	"X":     "Hello from the other side.",
+	// }
 
-	if x, ok := a["X"]; ok {
-		fmt.Println(x)
-	}
-	for key, val := range a {
-		fmt.Println(key, ":", val)
-	}
+	// if x, ok := a["X"]; ok {
+	// 	fmt.Println(x)
+	// }
+	// for key, val := range a {
+	// 	fmt.Println(key, ":", val)
+	// }
+	// Pointer
+	// a := 10
+	// fmt.Println(a)
+	// ptrA := &a
+	// fmt.Println(ptrA)
+	// fmt.Println(*ptrA)
+	// *ptrA = 15
+	// fmt.Println(a)
 
 }
