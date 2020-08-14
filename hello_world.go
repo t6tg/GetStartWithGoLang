d0053d7f63dc70f3766e7152714b1d7c0508a0ee
thanawatgulati <thanawatgulatijames@gmail.com>
2020-08-14 11:33:43 +0700

    pointer

--- hello_world.go
+++ hello_world.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	var i int
 	var p *int
+	i = 10
 	fmt.Printf("i = %d\n", i)
 	fmt.Printf("Address i = %v\n", &i)
 	p = &i
-	fmt.Printf("P = %v\n", p)
+	fmt.Printf("Address i in P = %v\n", p)
+	fmt.Printf("Address of P = %v\n", &p)
 	fmt.Printf("value of P = %v\n", *p)
 }
