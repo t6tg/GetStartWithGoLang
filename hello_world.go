e815e0a7da0925f5c5b18141f6b606432f6efc35
thanawatgulati <thanawatgulatijames@gmail.com>
2020-08-14 16:25:02 +0700

    struct

--- hello_world.go
+++ hello_world.go
@@ -1,13 +1,30 @@
 package main
 
+// type person struct {
+// 	Name string
+// 	age  int
+// }
+
+// func (p person) mutatePerson(name string) {
+// 	p.Name = name
+// 	fmt.Println("inside mutate:", p)
+// }
+
 func main() {
+	// p1 := person{Name: "James"}
+	// fmt.Println(p1)
+	// p1.mutatePerson("Hacker")
+	// fmt.Println(p1)
+	// a := [5]int{}
+	// mutateArray(a[:]) // slice is a part of pointer
+	// fmt.Println(a)
+	// fmt.Println(add(1, 3))
 	// Variable
 	// a := map[string]string{
 	// 	"Hello": "Gopher",
 	// 	"Name":  "James",
 	// 	"X":     "Hello from the other side.",
 	// }
-
 	// if x, ok := a["X"]; ok {
 	// 	fmt.Println(x)
 	// }
@@ -23,4 +40,12 @@ func main() {
 	// *ptrA = 15
 	// fmt.Println(a)
 
+}
+func add(x, y int) int {
+	p := 1
+	return x + y + p
+}
+
+func mutateArray(a []int) {
+	a[0] = 10
 }
