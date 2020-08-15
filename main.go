cb040cede4dc7278bbc4f75e99c897f987562799
thanawatgulati <thanawatgulatijames@gmail.com>
2020-08-15 13:53:26 +0700

    http

--- main.go
+++ main.go
@@ -2,52 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
-)
-
-var (
-	arr1 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	arr2 = []int{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	"log"
+	"net/http"
 )
 
 func main() {
-	withGo()
+	err := http.ListenAndServe(":5050", http.HandlerFunc(mux))
+	log.Println(err)
 }
 
-func withGo() {
-	defer timer()()
-	chRes1 := make(chan int)
-	chRes2 := make(chan int)
-	go func() {
-		chRes1 <- sum(arr1)
-	}()
-	go func() {
-		chRes2 <- sum(arr2)
-	}()
-	fmt.Println("sum arr1 = ", <-chRes1)
-	fmt.Println("sum arr2 = ", <-chRes2)
-	// go sum(arr2)
-}
-
-func withoutGo() {
-	defer timer()()
-	fmt.Println("Sum arr1", sum(arr1))
-	fmt.Println("Sum arr2", sum(arr2))
-}
-
-func timer() func() {
-	t := time.Now()
-	return func() {
-		diff := time.Now().Sub(t)
-		fmt.Println(diff)
+func mux(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	switch r.URL.Path {
+	case "/":
+		indexHandler(w, r)
+	case "/about":
+		aboutHandler(w, r)
+	default:
+		http.NotFound(w, r)
 	}
 }
 
-func sum(a []int) int {
-	s := 0
-	for _, x := range a {
-		s += x
-		time.Sleep(time.Millisecond * 200)
-	}
-	return s
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Index Page"))
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About Page"))
+}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("404 Page Not Found"))
 }
