b0b05ba7b7f4b3e97bdb837931f944306c1dfc62
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-09 15:06:07 +0700

    variable

--- index.go
+++ index.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Variable in Golang
+	fmt.Println("Hello World.")
+	var name string = "James"
+	var boolean bool = true
+	var Integer int = 1
+	fmt.Printf("String: %s ,Boolean: %t ,Integet: %d\n", name, boolean, Integer)
 }
